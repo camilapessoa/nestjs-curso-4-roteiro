6dd329c9ad203a2e30535eac5c8a7de57f24ddcf
Antônio Evaldo <antonio.evaldo349@gmail.com>
2023-07-19 15:19:32 -0300

    Vídeo 2.4 - Autenticando um usuário

--- src/modulos/autenticacao/autenticacao.service.ts
+++ src/modulos/autenticacao/autenticacao.service.ts
@@ -1,8 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UsuarioService } from '../usuario/usuario.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AutenticacaoService {
+  constructor(private usuarioService: UsuarioService) {}
+
   async login(email: string, senhaDigitada: string) {
-    console.log(email, senhaDigitada);
+    const usuario = await this.usuarioService.buscaPorEmail(email);
+
+    const usuarioFoiAutenticado = await bcrypt.compare(
+      senhaDigitada,
+      usuario.senha,
+    );
+
+    if (!usuarioFoiAutenticado) {
+      throw new UnauthorizedException('O email ou a senha está incorreto.');
+    }
+
+    console.log('usuário autenticado!');
   }
 }
