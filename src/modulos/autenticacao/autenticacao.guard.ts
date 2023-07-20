aeea9f54375ab84a6c07767ddcdc79b83cb28c27
camilapessoa <camilabentespessoa@gmail.com>
2023-07-20 11:32:35 -0300

    Vídeo 3.2 - Obtendo JWT do cabeçalho

--- src/modulos/autenticacao/autenticacao.guard.ts
+++ src/modulos/autenticacao/autenticacao.guard.ts
@@ -1,8 +1,28 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Request } from 'express';
 
 @Injectable()
 export class AutenticacaoGuard implements CanActivate {
   canActivate(contexto: ExecutionContext): boolean {
-    return false;
+    //1
+    const requisicao = contexto.switchToHttp().getRequest();
+    const token = this.extrairTokenDoCabecalho(requisicao);
+    //3
+    if (!token) {
+      throw new UnauthorizedException('Erro de autenticação');
+    }
+
+    return true;
+  }
+  //2
+  private extrairTokenDoCabecalho(requisicao: Request): string | undefined {
+    //formato do cabeçalho authorizathon: "Bearer <valor_do_jwt>" -> protocolo HTTP
+    const [tipo, token] = requisicao.headers.authorization?.split(' ') ?? [];
+    return tipo === 'Bearer' ? token : undefined;
   }
 }
