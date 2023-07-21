f533e2fa03e946a80891ded7e26befd72d4c33cc
camilapessoa <camilabentespessoa@gmail.com>
2023-07-21 12:10:38 -0300

    adição ao 3.3 useguards

--- src/modulos/pedido/pedido.controller.ts
+++ src/modulos/pedido/pedido.controller.ts
@@ -13,12 +13,12 @@ import { AtualizaPedidoDto } from './dto/AtualizaPedido.dto';
 import { CriaPedidoDTO } from './dto/CriaPedido.dto';
 import { AutenticacaoGuard } from '../autenticacao/autenticacao.guard';
 
+@UseGuards(AutenticacaoGuard)
 @Controller('pedidos')
 export class PedidoController {
   constructor(private readonly pedidoService: PedidoService) {}
 
   @Post()
-  @UseGuards(AutenticacaoGuard)
   async criaPedido(
     @Query('usuarioId') usuarioId: string,
     @Body() dadosDoPedido: CriaPedidoDTO,
